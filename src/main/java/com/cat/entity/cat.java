a7dd4eb5c6cdc0378217cfdf7398d61ca4c412af
xieshizhang <157760642@qq.com>
2024-04-08 23:29:44 +0800

    项目的配置文件以及实体类

--- src/main/java/com/cat/entity/cat.java
+++ src/main/java/com/cat/entity/cat.java
@@ -1,9 +1,27 @@
-package com.cat.entity;/**
-*@ClassName: cat
-*@Description: 猫的实体类
-*@Author: xieshizhang
-*@CreateTime: 2024-04-08 22:53
-*@Version: 1.0
-**/                               
+package com.cat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName: cat
+ * @Description: 猫的实体类
+ * @Author: xieshizhang
+ * @CreateTime: 2024-04-08 22:53
+ * @Version: 1.0
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class cat {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String color;
+    private String breed;
+    private String description;
+    private String location;
+
 }
