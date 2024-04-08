620f6598be6f11ac4bd94b12aaa85c83aeb84a0a
xieshizhang <157760642@qq.com>
2024-04-08 23:23:01 +0800

    Initial commit

---
+++ src/main/java/com/cat/entity/cat.java
@@ -0,0 +1,9 @@
+package com.cat.entity;/**
+*@ClassName: cat
+*@Description: 猫的实体类
+*@Author: xieshizhang
+*@CreateTime: 2024-04-08 22:53
+*@Version: 1.0
+**/                               
+public class cat {
+}
